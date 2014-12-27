4a1d0a4a535ec56cc5919dc8d1d31597eb680237
John Peck <john@eventuallabs.com>
2014-12-27 10:25:45 -0600

    Trimmed out loggerconf

--- implement/bitdecode.tcl
+++ implement/bitdecode.tcl
@@ -1,5 +1,8 @@
 
 
+# By default, the software will look for the configuration file in the
+# directory from which it was launched.  If the configuration file is
+# not found, one will be created.
 set configfile "[file rootname $argv0].cfg"
 
 # This software's version
@@ -39,89 +42,7 @@ proc modinfo {modname} {
 # logging.
 
 package require logger
-
-# Set up a visual console for logging
-frame .console_frme -relief groove
-# Set up the text widget.  Specify -width in units of characters in
-# the -font option
-text .console_text -yscrollcommand {.console_scrl set} \
-    -width 100 \
-    -height 10 \
-    -font LogFont
-# Use yview for a vertical scrollbar -- scrolls in the y direction
-# based on input
-scrollbar .console_scrl -orient vertical -command {.console_text yview}
-
-# initialize logger subsystems
-# two loggers are created
-# 1. main
-# 2. a separate logger for plugins
-set log [logger::init main]
-set log [logger::init global]
-${::log}::setlevel $loglevel; # Set the log level
-
-
-
-proc log_to_file {txt} {
-    upvar #0 argv0 thisScript; # Associate thisScript with the argv0
-    set logfile "[file rootname $thisScript].log"
-    set f [open $logfile {WRONLY CREAT APPEND}] ;# instead of "a"
-    fconfigure $f -encoding utf-8
-    puts $f $txt
-    close $f
-}
-
-# Send log messages to wherever they need to go
-proc log_manager {lvl txt} {
-    set msg "\[[clock format [clock seconds]]\] $txt \n"
-    # The logfile output
-    log_to_file $msg
-    
-    # The console logger output.  Mark the level names and color them
-    # after the text has been inserted.
-    if {[string compare $lvl debug] == 0} {
-	# Debug level logging
-    	set msg "\[ $lvl \] $txt \n"
-    	.console_text insert end $msg
-    	.console_text tag add debugtag \
-	    {insert linestart -1 lines +2 chars} \
-	    {insert linestart -1 lines +7 chars}
-    	.console_text tag configure debugtag -foreground blue
-    }
-    if {[string compare $lvl info] == 0} {
-	# Info level logging
-    	set msg "\[ $lvl \] $txt \n"
-    	.console_text insert end $msg
-    	.console_text tag add infotag \
-	    {insert linestart -1 lines +2 chars} \
-	    {insert linestart -1 lines +7 chars}
-    	.console_text tag configure infotag -foreground green
-    }
-    if {[string compare $lvl warn] == 0} {
-	# Warn level logging
-    	set msg "\[ $lvl \] $txt \n"
-    	.console_text insert end $msg
-    	.console_text tag add warntag \
-	    {insert linestart -1 lines +2 chars} \
-	    {insert linestart -1 lines +7 chars}
-    	.console_text tag configure warntag -foreground orange
-    }
-    if {[string compare $lvl error] == 0} {
-	# Error level logging
-    	set msg "\[ $lvl \] $txt \n"
-    	.console_text insert end $msg
-    	.console_text tag add errortag \
-	    {insert linestart -1 lines +2 chars} \
-	    {insert linestart -1 lines +7 chars}
-    	.console_text tag configure errortag -foreground red
-    } 
-}
-
-# Define the callback function for the logger for each log level
-foreach lvl [logger::levels] {
-    interp alias {} log_manager_$lvl {} log_manager $lvl
-    ${log}::logproc $lvl log_manager_$lvl
-}
+source loggerconf.tcl
 
 ${log}::info [modinfo logger]
 
@@ -138,6 +59,36 @@ ${log}::error "Error message"
 
 package require inifile
 ${log}::info [modinfo inifile]
+
+
+proc config_write {configfile} {
+    # Write a configuration file
+    #
+    # Arguments:
+    #   configfile -- Configuration file name (with path)
+    global log
+    global revcode
+    set fcon [ini::open $configfile w]
+    ini::set $fcon private version $revcode
+    ini::comment $fcon private "" "Internal use -- do not edit."
+    ini::set $fcon junk key "Some junk"
+    ini::comment $fcon junk "" "Some comment about junk section."
+    ini::set $fcon crap newkey "Some crap"
+    ini::comment $fcon crap "" "Some comment about crap section."
+    ini::comment $fcon crap newkey "Some comment about newkey."
+    ini::commit $fcon
+    ini::close $fcon
+    
+}
+
+if {[file exists $configfile] == 0} {
+    # The config file does not exist
+    ${log}::info "Creating new configuration file $configfile"
+    set fcon [ini::open $configfile w]
+    ini::close $fcon
+    # config_write $configfile
+}
+
 
 
 # ------------------------ Set up buttons -----------------------------
@@ -223,31 +174,6 @@ pack .entry_frme.0x_labl -side left
 pack .calc_butt -expand 1 -pady 5; # The calculate button
 
 
-proc configfile_init {filename} {
-    upvar #0 argv0 thisScript; # Associate thisScript with the argv0
-    upvar 1 log inlog
-    upvar 1 revcode inrevcode
-    set configfile $filename
-    set fout [open $configfile w]
-    fconfigure $fout -encoding utf-8
-    set scriptname [file rootname [file tail $thisScript]]
-    ${inlog}::info "Writing configuration file $configfile"
-    puts $fout "\# Configuration file for $scriptname"
-    puts $fout "\# This file is simply sourced by the tcl interpreter"
-    puts $fout ""
-    puts $fout "\# Software version that wrote this configuration file"
-    puts $fout "set config.version $inrevcode"
-    close $fout
-}
-
-# Write a configuration file if none exists
-if {[file exists $configfile] == 0} {
-    # The config file does not exist
-    configfile_init $configfile
-} else {
-    source $configfile
-    ${log}::debug "Configuration file version is ${config.version}"
-}
 
 
 #  Define a procedure - an action for Help-About
